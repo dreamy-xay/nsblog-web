4e1c9b2d6b7315d671156a92daef746ce77ece1a
dreamy-xay <1985332264@qq.com>
2021-06-09 13:48:39 +0800

    add menu, focus

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="admin">
-
+    <admin-top-bar></admin-top-bar>
+    <admin-menu></admin-menu>
   </div>
 </template>
 
 <script lang="ts">
 import { verifyToken } from '@/network/token';
+import adminMenu from '@/views/admin/childComs/adminMenu.vue';
+import adminTopBar from '@/views/admin/childComs/adminTopBar.vue';
 
 export default {
   name: 'Admin',
   beforeCreate() {
+    // return;
     if (!verifyToken().status) (this as any).$router.replace({ path: '/admin/login' });
+  },
+  components: {
+    adminMenu,
+    adminTopBar,
   },
 };
 </script>
