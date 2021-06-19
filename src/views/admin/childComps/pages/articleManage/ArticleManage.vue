c994d0367e51f1c60320acd032ac06a98661b029
KeepCoding <1710256504@qq.com>
2021-06-19 20:28:36 +0800

    update ArticleManage

--- src/views/admin/childComps/pages/articleManage/ArticleManage.vue
+++ src/views/admin/childComps/pages/articleManage/ArticleManage.vue
@@ -3,8 +3,8 @@
  * @Version:
  * @Autor: dreamy-xay
  * @Date: 2021-06-12 23:58:01
- * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-06-19 13:40:04
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-06-19 20:22:04
 -->
 
 <template>
@@ -13,14 +13,117 @@
     title="文章"
     ref="adminWindow"
   >
-    <!-- 文章数据表单
+    <!-- 功能模块栏 -->
+    <div class="article-manage-function-row">
+      <el-row :gutter="6">
+        <el-col :span="1.5">
+          <el-button
+            type="success"
+            size="small"
+            plain
+            @click="showAllArticle()"
+          >ALL</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="success"
+            size="small"
+            plain
+            @click="showDraft()"
+          >草稿</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="success"
+            size="small"
+            plain
+            @click="showReleased()"
+          >发布</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="danger"
+            size="small"
+            plain
+            @click="showGarbage()"
+          >垃圾箱</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="primary"
+            size="small"
+            plain
+            @click="showRecommend()"
+          >推荐</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="primary"
+            size="small"
+            plain
+            @click="showRecommend()"
+          >置顶</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-dropdown>
+            <el-button
+              type="warning"
+              size="small"
+              plain
+            >
+              分类<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu
+              :categories="categories"
+              @command="showCheckedCategory(item)"
+            >
+              <el-dropdown-item
+                v-for="catagory in categories"
+                :key="catagory.id"
+              >{{catagory.name}}</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="danger"
+            size="small"
+            icon="el-icon-delete-solid"
+            @click="deleteAllChecked()"
+          >删除</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="goon"
+            size="small"
+            icon="el-icon-s-tools"
+            color="#1AB394"
+            @click="SetTopAllChecked()"
+          >设为置顶</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="success"
+            size="small"
+            icon="el-icon-s-tools"
+            @click="recommendAllChecked()"
+          >设为推荐</el-button>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- 文章数据表单 -->
     <div class="article-manage-table">
       <el-table
-        class="el-table-article"
         :data="articles"
         :cell-style="cellStyle(row,column,rowIndex,columnIndex)"
         :header-cell-style="{background:'#313348',color:'#606266'}"
       >
+        <el-table-column
+          type="selection"
+          width="55"
+        >
+        </el-table-column>
+        <el-table-column type="index"></el-table-column>
         <el-table-column
           prop="title"
           label="文章标题"
@@ -45,6 +148,25 @@
         >
         </el-table-column>
         <el-table-column
+          prop="recommend"
+          label="博主推荐"
+        >
+          <template slot-scope="scope1">
+            <el-tag
+              v-if="scope1.row.recommend"
+              type="success"
+              effect="dark"
+              size="mini"
+            >是</el-tag>
+            <el-tag
+              v-else
+              type="info"
+              size="mini"
+              effect="dark"
+            >否</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
           prop="tag"
           label="标签"
         >
@@ -53,7 +175,7 @@
               v-for="item in scope1.row.tag"
               :key="item"
               size="small"
-              style="marginRight:6px"
+              style="margin-right: 6px; margin-bottom :4px"
             >{{item}}</el-tag>
           </template>
         </el-table-column>
@@ -66,7 +188,7 @@
               v-for="item in scope1.row.category"
               :key="item"
               size="small"
-              style="marginRight:6px"
+              style="margin-right: 6px; margin-bottom :4px"
             >{{item}}</el-tag>
           </template>
         </el-table-column>
@@ -116,6 +238,7 @@
         </el-table-column>
       </el-table>
     </div>
+    <!-- 分页导航栏 -->
     <div class="article-manage-pagination">
       <el-pagination
         @size-change="handleSizeChange"
@@ -128,7 +251,7 @@
         style="marginTop:12px"
       >
       </el-pagination>
-    </div> -->
+    </div>
   </admin-window>
 </template>
 
@@ -144,6 +267,24 @@ export default Vue.extend({
   name: 'articleManage',
   data() {
     return {
+      categories: [
+        {
+          id: 1,
+          name: 'java',
+        },
+        {
+          id: 2,
+          name: 'python',
+        },
+        {
+          id: 3,
+          name: '数据库',
+        },
+        {
+          id: 4,
+          name: 'vue',
+        },
+      ],
       articles: [
         {
           id: 1,
@@ -151,6 +292,7 @@ export default Vue.extend({
           type: '博文',
           publishTime: '2020-2-3',
           pageView: 12,
+          recommend: true,
           tag: ['标签1', '标签2'],
           category: ['分类1', '分类2'],
           publishStatus: '已发布',
@@ -161,6 +303,7 @@ export default Vue.extend({
           type: '博文',
           publishTime: '2020-2-3',
           pageView: 12,
+          recommend: false,
           tag: ['标签1', '标签2'],
           category: ['分类1', '分类2'],
           publishStatus: '已发布',
@@ -177,6 +320,26 @@ export default Vue.extend({
     };
   },
   methods: {
+    // 显示所有文章
+    showAllArticle() {},
+    // 显示草稿
+    showDraft() {},
+    // 显示已发布文章
+    showReleased() {},
+    // 显示垃圾箱中的文章
+    showGarbage() {},
+    // 显示推荐的文章
+    showRecommend() {},
+    // 显示置顶的文章
+    showTop() {},
+    // 显示指定分类的文章
+    showCheckedCategory(item: string) {},
+    // 删除选中的所有文章
+    deleteAllChecked() {},
+    // 置顶选中的所有文章
+    SetTopAllChecked() {},
+    // 推荐选中的所有文章
+    recommendAllChecked() {},
     // 编辑文章
     articleEdit(aid: number) {},
     // 删除文章
@@ -211,8 +374,13 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .atricle-manage {
+  .article-manage-function-row {
+    width: 100%;
+  }
+
   .atricle-manage-table {
     width: 100%;
+    margin-top: 20px !important;
     overflow: hidden;
   }
 
@@ -224,9 +392,18 @@ export default Vue.extend({
 </style>
 
 <style lang="scss">
+// 解决表头右侧边界样式问题
 .article-manage {
   .el-table__header-wrapper > table {
     background-color: #313348 !important;
   }
+}
+
+.el-button--goon,
+.el-button--goon:hover,
+.el-button--goon:focus {
+  color: azure;
+  background-color: #1ab394;
+  border-color: #1ab394;
 }
 </style>
