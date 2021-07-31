388f66607be19461141529d1cc85852b050d42b9
Z_Y_C <2918765917@qq.com>
2021-07-31 23:42:26 +0800

    修复了路由和::v-deep警告

--- src/views/message/childComps/pages/MessageSetting.vue
+++ src/views/message/childComps/pages/MessageSetting.vue
@@ -4,7 +4,7 @@
  * @Autor: Z_Y_C
  * @Date: 2021-07-28 13:02:11
  * @LastEditors: Z_Y_C
- * @LastEditTime: 2021-07-31 20:35:45
+ * @LastEditTime: 2021-07-31 23:40:09
 -->
 <template>
   <div
@@ -96,7 +96,7 @@ $green0: $green-0;
 $green1: $green-1;
 $shadow2: $shadow-2;
 
-::v-deep .el-radio {
+::v-deep(.el-radio) {
   color: $grey7;
   .el-radio__inner {
     width: 20px;
