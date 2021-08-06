a532b947377eeb035a62309851d99d8e54febb79
continue <1300877092@qq.com>
2021-08-06 09:33:27 +0800

    test

---
+++ src/components/content/baseTopBar/TopBarchild/TopBarHeadRight.vue
@@ -0,0 +1,104 @@
+<!--
+ * @Description:  首页顶部之右边部分组件
+ * @Version:
+ * @Autor: Ban
+ * @Date: 2021-07-20 11:01:33
+ * @LastEditors: continue-hs
+ * @LastEditTime: 2021-08-05 22:01:46
+-->
+
+<template>
+  <div class="base-top-bar-right">
+    <ul class="base-top-bar-right-menu">
+      <li>
+        <TopBarAvatar />
+      </li>
+      <li>
+        <TopBarMessage />
+      </li>
+      <li>动态</li>
+      <li>
+        <TopBarCollection />
+      </li>
+      <li>历史</li>
+      <li>创作中心</li>
+      <li>
+        <TopBarNoteShare />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+/**
+ * @description:  The right of TopBar
+ * @param {*}
+ * @return {*}
+ * @author: Ban
+ */
+import { ref } from 'vue';
+import { defineComponent } from '@vue/runtime-core';
+import TopBarAvatar from '@/components/content/baseTopBar/TopBarchild/TopBarAvatar.vue';
+import TopBarNoteShare from '@/components/content/baseTopBar/TopBarchild/TopBarNoteShare.vue';
+import TopBarMessage from '@/components/content/baseTopBar/TopBarchild/TopBarMessage.vue';
+import TopBarCollection from '@/components/content/baseTopBar/TopBarchild/TopBarCollection.vue';
+
+export default defineComponent({
+  name: 'TopBarHeadRight',
+  components: {
+    TopBarAvatar,
+    TopBarNoteShare,
+    TopBarMessage,
+    TopBarCollection,
+  },
+  setup() {
+    let isNoteshare = ref(true);
+    return {
+      isNoteshare,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+$green0: $green-0;
+$green1: $green-1;
+$grey0: $grey-0;
+.base-top-bar-right {
+  display: flex;
+  line-height: 56px;
+  .base-top-bar-right-menu {
+    height: 58px;
+    display: inline-block;
+    letter-spacing: 1px;
+    li {
+      display: inline-block;
+      vertical-align: middle;
+      font-size: 16px;
+      padding: 0 13px;
+      cursor: pointer;
+      &:hover {
+        color: $green0;
+      }
+    }
+    .noteshare {
+      display: inline-block;
+      background: $green0;
+      text-align: center;
+      height: 36px;
+      width: 96px;
+      line-height: 36px;
+      border-radius: 3px;
+      color: $grey0;
+      cursor: pointer;
+      position: relative;
+      &:hover {
+        background: $green1;
+      }
+      li {
+        color: $grey0;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/content/baseTopBar/TopBarchild/TopBarNoteShare.vue
@@ -0,0 +1,118 @@
+<!--
+ * @Description:   TopBarNoteShare
+ * @Version:
+ * @Autor: Ban
+ * @Date: 2021-07-29 15:53:25
+ * @LastEditors: continue-hs
+ * @LastEditTime: 2021-08-05 09:13:07
+-->
+<template>
+
+  <el-popover
+    placement="bottom"
+    :width="288"
+    trigger="click"
+    popper-class="el-noteshare"
+    style="padding : 0"
+  >
+    <a
+      href=""
+      class="noteshare-item"
+      v-for="item in noteshare"
+      :key="item.name"
+    >
+      <i
+        class="iconfont"
+        :class="item.iconfont"
+      ></i>
+      <div>
+        {{  item.name  }}
+      </div>
+    </a>
+    <template #reference>
+      <el-button
+        @click="visible = true"
+        class="noteShare"
+      >笔记分享</el-button>
+    </template>
+  </el-popover>
+</template>
+
+<script>
+import { fromBase64 } from 'js-base64';
+/**
+ * @description:  TopBar_笔记分享
+ * @param {*}
+ * @return {*}
+ * @author: Ban
+ */
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  setup() {
+    const noteshare = [
+      {
+        name: '写笔记',
+        iconfont: 'blog-edit-article',
+      },
+      {
+        name: '提问题',
+        iconfont: 'blog-wenti',
+      },
+      {
+        name: '享资源',
+        iconfont: 'blog-shangchuan',
+      },
+      {
+        name: '传图片',
+        iconfont: 'blog-icons01',
+      },
+    ];
+    return {
+      noteshare,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.noteShare {
+  background: $green-0;
+  color: $grey-0;
+  font-size: 16px;
+  border: 0;
+
+  &:hover {
+    background: $green-1;
+  }
+}
+
+.noteshare-item {
+  display: inline-block;
+  height: 72px;
+  width: 72px;
+  color: $grey-9;
+  text-align: center;
+  font-weight: 700;
+
+  &:hover {
+    background: $grey-2;
+    color: $green-0;
+  }
+
+  i.iconfont {
+    display: inline-block;
+    margin-top: 2px;
+    font-size: 32px;
+    font-weight: 400;
+  }
+}
+
+.el-popper {
+  padding: 0;
+}
+</style>
+<style>
+.el-noteshare {
+  padding: 0px !important;
+}
+</style>
