74e675adb0e37cb81427fb21351828b55c6f528e
dreamy-xay <1985332264@qq.com>
2021-08-10 20:26:21 +0800

    fix some api

--- src/network/api/collections.ts
+++ src/network/api/collections.ts
@@ -4,14 +4,14 @@
  * @Autor: dreamy-xay
  * @Date: 2021-08-03 12:55:44
  * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-08-10 15:32:36
+ * @LastEditTime: 2021-08-10 20:03:18
  */
 
 import { get, RequestLifeCycle } from '@/network/request';
 
 /**
  * @description: 获取收藏信息
- * @param {string} username `必传参数`
+ * @param {string} username 用户名 `必传参数`
  * @param {number} limit 返回collections的最大数量 `默认位10`
  * @param {number} offset 数据库收藏列表偏移量 `默认为0`
  * @param {number} favorites 收藏夹id `默认为''，返回全部收藏夹及其内容，传入id则返回指定收藏夹id内容`
@@ -30,6 +30,7 @@ export function getCollections(
     url: '/collections',
     ...RLC,
     params: {
+      username,
       limit,
       offset,
       favorites
