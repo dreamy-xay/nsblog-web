d9319968a04784926c6c2740713156d658937029
dreamy-xay <1985332264@qq.com>
2021-08-11 19:05:16 +0800

    fix some bugs, add socket store

--- src/store/modules/message.ts
+++ src/store/modules/message.ts
@@ -4,28 +4,24 @@
  * @Autor: dreamy-xay
  * @Date: 2021-08-11 11:45:25
  * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-08-11 17:09:02
+ * @LastEditTime: 2021-08-11 19:02:08
  */
 
 import { StoreOptions } from 'vuex';
+import socket from '@/socket';
 
 const messageStore: StoreOptions<unknown> = {
-  state: {
-    message: '9999'
-  },
-  getters: {
-    messages(state: any) {
-      return state.message + 'x';
-    }
-  },
-  mutations: {
-    setMessage(state: any, m: string) {
-      state.message = m;
-    }
-  },
+  state: {},
+  getters: {},
+  mutations: {},
   actions: {
-    log({ state }, xx: any) {
-      console.log(xx);
+    receiveMessage({ state }, options: { type: number; callback: (data: Record<string, unknown>) => void }) {
+      socket.on('messageNotice', (type: number, data: Record<string, unknown>) => {
+        if (options.type === type) options.callback(data);
+      });
+    },
+    online({ state }, username: string) {
+      socket.emit('online', username);
     }
   }
 };
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,7 +4,7 @@
  * @Autor: dreamy-xay
  * @Date: 2021-06-09 08:19:13
  * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-08-11 17:44:11
+ * @LastEditTime: 2021-08-11 19:03:08
 -->
 
 <template>
@@ -16,7 +16,7 @@
 <script>
 import { defineComponent } from 'vue';
 import BaseTopBar from '@/components/content/baseTopBar/BaseTopBar';
-import socket from '@/socket';
+import { mapActions } from '@/util/store';
 
 /**
  * @description: 博客主页
@@ -29,9 +29,46 @@ export default defineComponent({
     BaseTopBar,
   },
   mounted() {
+    const { online, receiveMessage } = mapActions('message', ['receiveMessage', 'online']);
+
+    // 用户上线了
     setTimeout(() => {
-      socket.emit('online', 'dreamy');
+      online('dreamy');
     }, 3000);
+
+    // 收到消息
+    // 类型1
+    receiveMessage({
+      type: 1,
+      callback(data) {
+        console.log('message type: 1');
+        console.log(data);
+      },
+    });
+    // 类型1
+    receiveMessage({
+      type: 2,
+      callback(data) {
+        console.log('message type: 2');
+        console.log(data);
+      },
+    });
+    // 类型1
+    receiveMessage({
+      type: 3,
+      callback(data) {
+        console.log('message type: 3');
+        console.log(data);
+      },
+    });
+    // 类型1
+    receiveMessage({
+      type: 4,
+      callback(data) {
+        console.log('message type: 4');
+        console.log(data);
+      },
+    });
   },
 });
 </script>
