21c035d101a243dbfcbddb74a9481ffd1ce518e3
dreamy-xay <1985332264@qq.com>
2021-08-11 16:01:43 +0800

    fix some bugs, add api

--- src/plugins/native-ui.ts
+++ src/plugins/native-ui.ts
@@ -3,13 +3,13 @@
  * @Version:
  * @Autor: dreamy-xay
  * @Date: 2021-08-03 16:13:02
- * @LastEditors: Z_Y_C
- * @LastEditTime: 2021-08-09 22:38:42
+ * @LastEditors: dreamy-xay
+ * @LastEditTime: 2021-08-11 15:46:03
  */
 import { App } from 'vue';
-import { create, NMessageProvider, NBadge, NButton } from 'naive-ui';
+import { create, NMessageProvider, NModal, NBadge, NButton } from 'naive-ui';
 
-const components = [NMessageProvider, NBadge, NButton];
+const components = [NMessageProvider, NModal, NBadge, NButton];
 
 export default (app: App): void => {
   const naive = create({
