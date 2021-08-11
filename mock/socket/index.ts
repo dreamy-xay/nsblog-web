e5bebf3e642c4dc2c09fa63b5d571ffa14c58089
dreamy-xay <1985332264@qq.com>
2021-08-11 19:05:16 +0800

    fix some bugs, add socket store

--- mock/socket/index.ts
+++ mock/socket/index.ts
@@ -4,8 +4,9 @@
  * @Autor: dreamy-xay
  * @Date: 2021-08-10 21:44:12
  * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-08-11 13:52:09
+ * @LastEditTime: 2021-08-11 18:59:03
  */
+import { Random } from 'better-mock';
 import { Server } from 'http';
 import * as socketIo from 'socket.io';
 
@@ -17,17 +18,18 @@ export default (server: Server) => {
     }
   });
 
-  let onlineUsers = {};
-  let onlineCount = 0;
+  const onlineUsers: Record<string, string> = {};
+  let onlineCount: number = 0;
 
   io.on('connection', socket => {
-    let toUser = null;
-    let fromUser = null;
-    let msg = null;
-    let currentUser = null;
+    let toUser: string = null;
+    let fromUser: string = null;
+    let msg: string = null;
+    let currentUser: string = null;
 
+    // 聊天
     socket.on('online', userId => {
-      if (!(userId in onlineUsers)) {
+      if (!Object.prototype.hasOwnProperty.call(onlineUsers, userId)) {
         onlineUsers[userId] = socket.id;
         onlineCount++;
         currentUser = userId;
@@ -43,23 +45,74 @@ export default (server: Server) => {
         msg = obj.message;
 
         if (toUser === 'allUser') {
-          for (const user in onlineUsers) {
+          for (const user in onlineUsers)
             if (user != fromUser) io.to(onlineUsers[user]).emit('receiveMessage', msg);
-          }
-        } else if (toUser in onlineUsers) {
-          io.to(onlineUsers[toUser]).emit('receiveMessage', msg);
-        } else {
-          console.log(toUser + '不在线');
+            else if (toUser in onlineUsers) io.to(onlineUsers[toUser]).emit('receiveMessage', msg);
+            else console.log(toUser + '不在线');
         }
       });
 
       socket.on('disconnect', () => {
-        onlineCount--;
-        console.log('离开一人(' + currentUser + ')，在线人数：', onlineCount);
-        delete onlineUsers[currentUser];
-        currentUser = null;
-        console.log(onlineUsers);
+        if (Object.prototype.hasOwnProperty.call(onlineUsers, currentUser)) {
+          --onlineCount;
+          console.log('离开一人(' + currentUser + ')，在线人数：', onlineCount);
+          delete onlineUsers[currentUser];
+          currentUser = null;
+          console.log(onlineUsers);
+        }
       });
     });
+
+    // 消息
+    function getRandom(type: number): Record<string, unknown> {
+      let content: unknown;
+      if (type === 1) content = Random.integer(0, 1) ? Random.cparagraph(1, 10) : Random.paragraph(1, 10);
+      else if (type === 2)
+        content = {
+          username: Random.word(4, 8),
+          nickname: Random.natural(0, 1000000) % 2 ? Random.cword(2, 4) : Random.word(4, 8),
+          avatar: Random.image('150x150', '#234567', '#FFFFFF', 'png', Random.word(2, 4)),
+          content: Random.integer(0, 1) ? Random.cparagraph(1, 10) : Random.paragraph(1, 10),
+          type: Random.natural(1, 2),
+          reply_username: Random.word(4, 8),
+          reply_content: Random.integer(0, 1) ? Random.cparagraph(1, 10) : Random.paragraph(1, 10)
+        };
+      else if (type === 3)
+        content = {
+          username: Random.word(4, 8),
+          nickname: Random.natural(0, 1000000) % 2 ? Random.cword(2, 4) : Random.word(4, 8),
+          avatar: Random.image('150x150', '#234567', '#FFFFFF', 'png', Random.word(2, 4)),
+          type: Random.natural(1, 2),
+          id: Random.id()
+        };
+      else
+        content = {
+          username: Random.word(4, 8),
+          nickname: Random.natural(0, 1000000) % 2 ? Random.cword(2, 4) : Random.word(4, 8),
+          avatar: Random.image('150x150', '#234567', '#FFFFFF', 'png', Random.word(2, 4)),
+          attention: Random.natural(0, 1) ? true : false
+        };
+
+      return {
+        message_id: Random.increment(Random.integer(0, 10)),
+        content,
+        status: Random.integer(0, 1),
+        time: Random.datetime()
+      };
+    }
+    // 随机发送消息
+    function messageNotice(timeout: number = Random.natural(100, 50000)) {
+      const timer: NodeJS.Timeout = setTimeout(() => {
+        clearTimeout(timer);
+        const type: number = Random.natural(1, 4);
+        if (currentUser) {
+          io.to(onlineUsers[currentUser]).emit('messageNotice', type, getRandom(type));
+          console.log(`messageNotice:  type: ${type}`);
+          messageNotice();
+        }
+      }, timeout);
+    }
+
+    messageNotice();
   });
 };
