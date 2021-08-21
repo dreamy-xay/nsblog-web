b0092fe80533c279a687e773f95836d4f2bdd54b
dreamy-xay <1985332264@qq.com>
2021-08-21 14:32:57 +0800

    fix some files，fix some bugs

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@
  * @Autor: dreamy-xay
  * @Date: 2021-07-06 12:25:41
  * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-08-20 15:36:25
+ * @LastEditTime: 2021-08-21 14:20:15
  */
 
 const path = require('path');
