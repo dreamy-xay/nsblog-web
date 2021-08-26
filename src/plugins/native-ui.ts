5dec912daf76f77f8ded64bb76aa7c596fdfb80c
Z_Y_C <2918765917@qq.com>
2021-08-26 21:47:28 +0800

    '更新了组件'

--- src/plugins/native-ui.ts
+++ src/plugins/native-ui.ts
@@ -3,13 +3,13 @@
  * @Version:
  * @Autor: dreamy-xay
  * @Date: 2021-08-03 16:13:02
- * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-08-11 15:46:03
+ * @LastEditors: Z_Y_C
+ * @LastEditTime: 2021-08-26 20:40:24
  */
 import { App } from 'vue';
-import { create, NMessageProvider, NModal, NBadge, NButton } from 'naive-ui';
+import { create, NMessageProvider, NModal, NBadge, NButton, NPopover } from 'naive-ui';
 
-const components = [NMessageProvider, NModal, NBadge, NButton];
+const components = [NMessageProvider, NModal, NBadge, NButton, NPopover];
 
 export default (app: App): void => {
   const naive = create({
