55d53e9160dbdf15f0b1dc1760f1c1c05bd013d6
Z_Y_C <2918765917@qq.com>
2021-08-28 23:34:58 +0800

    '更新了个人资料'

---
+++ src/views/userCenter/childComps/pages/UserCenterProfileEdit/UserCenterProfileEdit.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Description:
+ * @Version:
+ * @Autor: Ban
+ * @Date: 2021-08-19 11:57:31
+ * @LastEditors: Z_Y_C
+ * @LastEditTime: 2021-08-28 23:23:25
+-->
+<template>
+  <user-center-profile-edit-information></user-center-profile-edit-information>
+  <user-center-profile-edit-job></user-center-profile-edit-job>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import userCenterProfileEditJob from '@/views/userCenter/childComps/pages/UserCenterProfileEdit/childComps/userCenterProfileEditJob.vue';
+import userCenterProfileEditInformation from '@/views/userCenter/childComps/pages/UserCenterProfileEdit/childComps/userCenterProfileEditInformation.vue';
+
+export default defineComponent({
+  name: 'UserCenterProfileEdit',
+  components: {
+    userCenterProfileEditJob,
+    userCenterProfileEditInformation,
+  },
+  setup(props, context) {
+    return {};
+  },
+});
+</script>
+
+<style lang='scss' scoped>
+</style>
