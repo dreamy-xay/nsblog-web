79aee046a73474381d9d9bb66965be66c080497e
dreamy-xay <1985332264@qq.com>
2021-08-30 12:27:31 +0800

    modify baseAvatarCopper.vue

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,29 +4,18 @@
  * @Autor: dreamy-xay
  * @Date: 2021-06-09 08:19:13
  * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-08-28 17:49:47
+ * @LastEditTime: 2021-08-30 12:26:59
 -->
 
 <template>
   <div class="home">
-    <base-top-bar v-model="show" />
-    <button @click="show = true">cropper image</button>
-    <base-avatar-cropper
-      v-model="show"
-      @uploaded="handleUploaded"
-      upload-url="/files/upload"
-    />
-    <img
-      v-if="userAvatar"
-      :src="userAvatar"
-    >
+    <base-top-bar />
   </div>
 </template>
 
 <script>
-import { defineComponent, ref, watch } from 'vue';
+import { defineComponent } from 'vue';
 import BaseTopBar from '@/components/content/baseTopBar/BaseTopBar.vue';
-import BaseAvatarCropper from '@/components/content/baseAvatarCropper/BaseAvatarCropper.vue';
 
 /**
  * @description: 博客主页
@@ -37,21 +26,6 @@ export default defineComponent({
   name: 'Home',
   components: {
     BaseTopBar,
-    BaseAvatarCropper,
-  },
-  setup() {
-    const show = ref(false);
-    const userAvatar = ref('');
-
-    function handleUploaded(resp) {
-      userAvatar.value = resp.relative_url;
-    }
-
-    return {
-      show,
-      userAvatar,
-      handleUploaded,
-    };
   },
 });
 </script>
