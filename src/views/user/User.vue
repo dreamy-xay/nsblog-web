96948278340b852784e0ed3f5adef7120f218063
dreamy-xay <1985332264@qq.com>
2021-09-07 18:16:35 +0800

    add user page

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -4,7 +4,7 @@
  * @Autor: dreamy-xay
  * @Date: 2021-09-07 16:04:40
  * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-09-07 16:26:45
+ * @LastEditTime: 2021-09-07 18:14:58
 -->
 <template>
   <base-view
@@ -12,12 +12,24 @@
     :background="true"
     bind-class="user"
   >
+    <div class="user-head">
+      <user-header />
+    </div>
+    <div class="user-body">
+      <user-info />
+      <user-main />
+    </div>
   </base-view>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import BaseView from '@/components/content/baseView/BaseView.vue';
+import UserHeader from '@/views/user/childComps/UserHeader.vue';
+import UserInfo from '@/views/user/childComps/userInfo/UserInfo.vue';
+import UserMain from '@/views/user/childComps/userMain/UserMain.vue';
+import { mapState } from '@/util/store';
+import { useRoute } from 'vue-router';
 
 /**
  * @description: 用户详细信息页面
@@ -28,12 +40,32 @@ export default defineComponent({
   name: 'user',
   components: {
     BaseView,
+    UserHeader,
+    UserInfo,
+    UserMain,
+  },
+  setup() {
+    const route = useRoute(); // route
+    const username = route.params.username; // 获取路由的username
+    const isSelf = mapState('global', ['tokenInfo']).tokenInfo.value.username === username; // 查询当下浏览器登录username
+    console.log(isSelf);
+    return {};
   },
 });
 </script>
 
 <style lang="scss" scoped>
 .user {
-  width: 100%;
+  .user-head {
+    width: 100%;
+    @include flex();
+    margin: 16px 0;
+  }
+
+  .user-body {
+    width: 100%;
+    @include flex(initial, space-between);
+    margin-bottom: 16px;
+  }
 }
 </style>
