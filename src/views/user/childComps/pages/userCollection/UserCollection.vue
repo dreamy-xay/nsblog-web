c3d76ac2c96720828740e9719502d0f8eb7f4a44
Z_Y_C <2918765917@qq.com>
2021-09-14 19:47:34 +0800

    update usercollection

--- src/views/user/childComps/pages/userCollection/UserCollection.vue
+++ src/views/user/childComps/pages/userCollection/UserCollection.vue
@@ -4,22 +4,60 @@
  * @Autor: dreamy-xay
  * @Date: 2021-09-07 16:24:57
  * @LastEditors: Z_Y_C
- * @LastEditTime: 2021-09-14 12:04:42
+ * @LastEditTime: 2021-09-14 19:40:03
 -->
 <template>
   <div class="user-collection">
     <div
       v-for="(item , index) in collectionData"
       :key=index
-      class="usr-collection-context"
-    >123</div>
+      class="user-collection-context"
+    >
+      <div class="user-collection-context-top">
+        <div class="icon1"><i class="iconfont blog-folder_special"></i></div>
+        <div class="text1">{{item.name}}</div>
+      </div>
+
+      <div class="user-collection-context-bottom">
+        <div class="first">
+          <div class="icon2"><i class="iconfont blog-fontAwesome_file"></i></div>
+          <div class="text2">{{item.count+'内容'}}</div>
+          <div class="line"></div>
+          <div
+            class="center"
+            role="button"
+            @click="changeOpenFavorites(index)"
+          >
+            <div class="text2">{{openFavorites[index] ? '收起' : '展开'}}</div>
+            <div class="icon3"><i
+                class="iconfont blog-FontAwesomechevroncircledown"
+                :class="openFavorites[index] ? 'rotation' : ''"
+              ></i></div>
+          </div>
+
+        </div>
+        <div class="last">
+          <div class="icon4"><i class="iconfont blog-lishijilu-copy"></i></div>
+          <div class="time">{{"创建于 "+dateFormat('YY-mm-dd HH:MM', new Date())}}</div>
+        </div>
+      </div>
+      <div
+        class="collection"
+        v-if="openFavorites[index]"
+      >
+        <show-collection :data="item.collections" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { getFavorites } from '@/network/api/favorites';
+import { dateFormat } from '@/util/date';
+import { useMessage } from 'naive-ui';
 import { defineComponent, reactive } from 'vue';
 import { useRoute } from 'vue-router';
+import ShowCollection from '@/views/user/childComps/pages/userCollection/childComps/ShowCollection.vue';
 
 /**
  * @description: 用户主页收藏记录
@@ -28,16 +66,35 @@ import { useRoute } from 'vue-router';
 
 export default defineComponent({
   name: 'userCollection',
+  components: {
+    ShowCollection,
+  },
   setup() {
-    const route = useRoute();
+    const msg = useMessage(); // naive-ui
+    const route = useRoute(); // 路由
     const collectionData = reactive([]);
     const username = route.params.username; // 获取路由的username
+    const openFavorites = reactive([]);
 
-    getFavorites(username, 5, 0).then((data) => {
-      collectionData.splice(0, 0, ...data.favorites);
-      console.log(collectionData);
-    });
-    return { collectionData };
+    getFavorites(username, 5, 0)
+      .then((data) => {
+        collectionData.splice(0, 0, ...data.favorites);
+        for (let i = 0; i < data.favorites.length; i++) {
+          openFavorites.push(false);
+        }
+        console.log(openFavorites);
+        console.log(collectionData);
+      })
+      .catch((error) => {
+        console.log(error);
+        msg.error('获取关注信息失败', { duration: 2000, closable: true });
+      });
+
+    function changeOpenFavorites(index) {
+      openFavorites[index] = !openFavorites[index];
+    }
+
+    return { collectionData, dateFormat, openFavorites, changeOpenFavorites };
   },
 });
 </script>
@@ -46,13 +103,116 @@ export default defineComponent({
 .user-collection {
   width: 100%;
   @include flex(initial, center, column);
-  .usr-collection-context {
-    height: 56px;
+
+  .user-collection-context {
     box-shadow: $shadow-0;
     border-radius: $border-radius-0;
     background-color: $grey-0;
     margin-bottom: 16px;
     padding: 16px;
+
+    .user-collection-context-top {
+      @include flex(center, initial, row);
+      margin-bottom: 16px;
+
+      .icon1 {
+        @include flex(center, initial);
+        width: 20px;
+        height: 20px;
+        margin-right: 8px;
+
+        .iconfont {
+          font-size: 20px;
+          color: $grey-9;
+        }
+      }
+
+      .text1 {
+        font-size: 14px;
+        height: 20px;
+        line-height: 20px;
+        color: $grey-10;
+      }
+    }
+
+    .user-collection-context-bottom {
+      @include flex(center, initial, row);
+
+      .first {
+        width: 658px;
+        @include flex(center, initial, row);
+        .icon2 {
+          @include flex(center);
+          width: 20px;
+          height: 20px;
+          margin-right: 8px;
+
+          .iconfont {
+            font-size: 20px;
+            color: $grey-7;
+          }
+        }
+
+        .text2 {
+          font-size: 14px;
+          height: 20px;
+          line-height: 20px;
+          color: $grey-10;
+        }
+
+        .line {
+          height: 14px;
+          border-left: 1px solid $grey-3;
+          margin: 0 16px;
+        }
+
+        .center {
+          @include flex(center, initial, row);
+          .icon3 {
+            @include flex(center);
+            width: 20px;
+            height: 20px;
+            margin-left: 8px;
+
+            .iconfont {
+              font-size: 19px;
+              color: $grey-7;
+            }
+
+            .rotation {
+              transform: rotate(180deg);
+            }
+          }
+        }
+      }
+
+      .last {
+        width: 200px;
+        @include flex(center, flex-end, row);
+
+        .icon4 {
+          @include flex(center);
+          width: 20px;
+          height: 20px;
+          margin-right: 8px;
+
+          .iconfont {
+            font-size: 20px;
+            color: $grey-7;
+          }
+        }
+
+        .time {
+          font-size: 14px;
+          height: 20px;
+          line-height: 20px;
+          color: $grey-7;
+        }
+      }
+    }
+    .collection {
+      margin-top: 16px;
+    }
   }
 }
 </style>
---
+++ src/views/user/childComps/pages/userCollection/childComps/ShowCollection.vue
@@ -0,0 +1,100 @@
+<!--
+ * @Description:展示收藏夹
+ * @Version:
+ * @Autor: Z_Y_C
+ * @Date: 2021-09-14 18:01:22
+ * @LastEditors: Z_Y_C
+ * @LastEditTime: 2021-09-14 19:45:44
+-->
+
+<template>
+  <div class="show-collection">
+    <div
+      v-for="(item , index) in data"
+      :key="index"
+      class="show-collection-context"
+    >
+      <div class="first">
+        <base-tag
+          :hollow="true"
+          :color="styles.pink0"
+          :size="22"
+          :text="item.type === 1 ?'文章' : ( item.type === 2 ? '问答' : '资源')  "
+          :style="{borderRadius: '11px'}"
+        />
+        <div
+          class="text"
+          role="button"
+        >{{item.title}}</div>
+      </div>
+
+      <div class="icon"><i class="iconfont blog-fav"></i></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import BaseTag from '@/components/content/baseTag/BaseTag.vue';
+import styles from '@/assets/style/define.scss';
+
+export default defineComponent({
+  name: 'showCollection',
+  components: { BaseTag },
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup() {
+    return { styles };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.show-collection {
+  width: 818px;
+  background-color: $grey-1;
+  box-shadow: $shadow-0;
+  border-radius: $border-radius-0;
+  padding: 24px 16px 16px 16px;
+  @include flex(initial, center, column);
+
+  .show-collection-context {
+    @include flex(center, initial, row);
+    height: 22px;
+    margin-bottom: 24px;
+
+    .first {
+      @include flex(center, initial, row);
+      width: 798px;
+
+      .text {
+        @include ellipsis(1);
+        font-size: 16px;
+        height: 22px;
+        line-height: 22px;
+        color: $grey-11;
+        margin-left: 12px;
+        transition: all 0.25s;
+
+        &:hover {
+          color: $pink-0;
+        }
+      }
+    }
+
+    .icon {
+      height: 22px;
+      width: 22px;
+      color: $orange-0;
+
+      .iconfont {
+        font-size: 22px;
+      }
+    }
+  }
+}
+</style>
