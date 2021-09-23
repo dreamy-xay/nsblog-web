e1d7f6f7ae22091d47eb2b6318b0304b35473760
dreamy-xay <1985332264@qq.com>
2021-09-23 20:28:30 +0800

    add some apis

--- mock/intercepter/articles.ts
+++ mock/intercepter/articles.ts
@@ -4,11 +4,11 @@
  * @Autor: dreamy-xay
  * @Date: 2021-09-13 21:24:06
  * @LastEditors: dreamy-xay
- * @LastEditTime: 2021-09-23 18:31:55
+ * @LastEditTime: 2021-09-23 20:25:20
  */
 import { Application, Request, Response } from 'express';
 import { Random } from 'better-mock';
-import { int } from './util';
+import { int, verifyToken, getToken, RandomUser, randomUsers } from './util';
 import select from '../data/index';
 
 export default function(baseUrl: string, app: Application) {
@@ -37,5 +37,128 @@ export default function(baseUrl: string, app: Application) {
       return ans;
     }
     return res.json({ articles: getRandom(int(offset) >= 78 ? 0 : Math.min(int(limit), 78 - int(offset))) });
+  });
+
+  // 获取用户文章所有分类
+  app.get(baseUrl + '/articles/categories', (req: Request, res: Response) => {
+    const { username } = req.query;
+    if (!select('users').findOne({ username })) return res.status(410).json({ error: 'User name error' });
+    console.log(`${username} getArticlesCategories... `);
+
+    function getRandom(limit: number): Record<string, unknown>[] {
+      const ans: Record<string, unknown>[] = [];
+      for (let i: number = 0; i < limit; ++i)
+        ans.push({ id: Random.id(), name: Random.integer(0, 1) ? Random.word() : Random.cword() });
+      return ans;
+    }
+
+    return res.json({ categories: getRandom(Random.integer(0, 40)) });
+  });
+
+  // 获取用户文章所有分类
+  app.get(baseUrl + '/articles/tags', (req: Request, res: Response) => {
+    const { username } = req.query;
+    if (!select('users').findOne({ username })) return res.status(410).json({ error: 'User name error' });
+    console.log(`${username} getArticlesTags... `);
+
+    function getRandom(limit: number): Record<string, unknown>[] {
+      const ans: Record<string, unknown>[] = [];
+      for (let i: number = 0; i < limit; ++i)
+        ans.push({ id: Random.id(), name: Random.integer(0, 1) ? Random.word() : Random.cword() });
+      return ans;
+    }
+
+    return res.json({ tags: getRandom(Random.integer(0, 40)) });
+  });
+
+  // 文章侧边栏菜单，获取详情
+  app.get(baseUrl + '/articles/users', (req: Request, res: Response) => {
+    const { username } = req.query;
+    if (!select('users').findOne({ username })) return res.status(410).json({ error: 'User name error' });
+    console.log(`${username} getArticlesUsersInfo... `);
+
+    function getRandom(limit: number, isRecent: boolean = true): Record<string, unknown>[] {
+      const ans: Record<string, unknown>[] = [];
+      for (let i: number = 0; i < limit; ++i)
+        if (isRecent)
+          ans.push({
+            title: Random.integer(0, 1) ? Random.title() : Random.ctitle(),
+            article_id: Random.id()
+          });
+        else
+          ans.push({
+            title: Random.integer(0, 1) ? Random.title() : Random.ctitle(),
+            link: Random.url()
+          });
+      return ans;
+    }
+
+    const user: RandomUser = randomUsers().random();
+    const ans: Record<string, unknown> = {
+      username: user.username,
+      nickname: user.nickname,
+      avatar: Random.image('150x150', '#234567', '#FFFFFF', 'png', user.username),
+      signature: (Random.integer(0, 1) ? Random.cparagraph(1, 1) : Random.paragraph(1, 1)).slice(0, 128),
+      article_count: Random.integer(0, 100),
+      category_count: Random.integer(0, 40),
+      tag_count: Random.integer(0, 40),
+      recent_article: getRandom(Random.integer(0, 10)),
+      friend_chain: getRandom(Random.integer(0, 40), false)
+    };
+
+    return res.json(ans);
+  });
+
+  // 修改文章状态，推荐反对还是不操作
+  app.put(baseUrl + '/articles/evaluation', (req: Request, res: Response) => {
+    if (!verifyToken(req.headers)) return res.status(401).json({ error: 'Unauthorized' });
+    const username: string = getToken(req.headers).username;
+    const { type, article_id } = req.body;
+    console.log(`${username} modifyArticleEvaluation... :   type=>${type}  article_id=>${article_id}`);
+    return res.status(200).send();
+  });
+
+  // 获取文章详情
+  app.get(baseUrl + '/articles/:article_id', (req: Request, res: Response) => {
+    let username: string = '';
+    if (verifyToken(req.headers)) username = getToken(req.headers).username;
+
+    const { article_id } = req.params;
+    console.log(`${username} getDetailArticles:  article_id${article_id}`);
+
+    function getRandom(limit: number): Record<string, unknown>[] {
+      const ans: Record<string, unknown>[] = [];
+      for (let i: number = 0; i < limit; ++i)
+        ans.push({ id: Random.id(), name: Random.integer(0, 1) ? Random.word() : Random.cword() });
+      return ans;
+    }
+
+    const params: Record<string, unknown> = username !== '' ? {} : {};
+
+    const ans: Record<string, unknown> = {
+      article_id,
+      title: Random.integer(0, 1) ? Random.title() : Random.ctitle(),
+      username: Random.name(),
+      nickname: Random.integer(0, 1) ? Random.name() : Random.cname(),
+      release_time: Random.datetime(),
+      page_view: Random.integer(0, 1000),
+      comment_count: Random.integer(0, 1000),
+      topic: Random.integer(0, 1) ? Random.word() : Random.cword(),
+      category: getRandom(Random.integer(0, 2)),
+      tag: getRandom(Random.integer(0, 3)),
+      content: Random.integer(0, 1) ? Random.paragraph(3, 100) : Random.cparagraph(3, 100),
+      recommend_count: Random.integer(0, 1000),
+      ...params,
+      last_article: {
+        article_id: Random.id(),
+        title: Random.integer(0, 1) ? Random.title() : Random.ctitle()
+      },
+      next_article: {
+        article_id: Random.id(),
+        title: Random.integer(0, 1) ? Random.title() : Random.ctitle()
+      }
+    };
+
+    return res.json(ans);
   });
 }
