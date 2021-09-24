d6bf14317d37d03bf45e1d5cec83ac6a52cb96e4
Z_Y_C <2918765917@qq.com>
2021-09-24 21:11:29 +0800

    'update'

---
+++ src/views/article/childComps/articleMenu/childComps/ArticleMenuAvatar.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Description: 文章目录头像
+ * @Version:
+ * @Autor: Z_Y_C
+ * @Date: 2021-09-24 20:40:42
+ * @LastEditors: Z_Y_C
+ * @LastEditTime: 2021-09-24 20:41:20
+-->
+<template>
+  <div class="article-menu-avatar">
+
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+/**
+ * @description:文章目录头像
+ * @author: Z_Y_C
+ */
+
+export default defineComponent({
+  name: 'articleMenuAvatar',
+});
+</script>
+
+<style lang="scss" scoped>
+.article-menu-avatar {
+}
+</style>
