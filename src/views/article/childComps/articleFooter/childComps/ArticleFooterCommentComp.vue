711aaaedecd5b495c5df0b88b09f014369b64df9
KeepCoding <1710256504@qq.com>
2021-09-25 19:07:47 +0800

    'update'

--- src/views/article/childComps/articleFooter/childComps/ArticleFooterCommentComp.vue
+++ src/views/article/childComps/articleFooter/childComps/ArticleFooterCommentComp.vue
@@ -4,7 +4,7 @@
  * @Autor: clq
  * @Date: 2021-09-23 19:22:16
  * @LastEditors: clq
- * @LastEditTime: 2021-09-25 16:15:30
+ * @LastEditTime: 2021-09-25 18:29:02
 -->
 <template>
   <div class="article-footer-comment-comp">
@@ -12,7 +12,7 @@
       <div class="left">
         <base-avatar
           :size="32"
-          :src="123"
+          src="http://123"
         />
       </div>
       <div class="right">
