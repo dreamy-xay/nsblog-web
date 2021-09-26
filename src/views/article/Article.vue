54b7e90c878eaac142e261a26d0a7f4696d5ab4d
KeepCoding <1710256504@qq.com>
2021-09-26 17:02:55 +0800

    'update'

--- src/views/article/Article.vue
+++ src/views/article/Article.vue
@@ -4,7 +4,7 @@
  * @Autor: dreamy-xay
  * @Date: 2021-09-16 16:32:13
  * @LastEditors: clq
- * @LastEditTime: 2021-09-25 20:31:14
+ * @LastEditTime: 2021-09-26 16:52:50
 -->
 
 <template>
@@ -68,9 +68,11 @@ export default defineComponent({
       tags: [],
       content: '',
       recommend_count: 0,
-      evaluation: 0,
-      collection: 0,
-      attention: 0,
+
+      evaluation: null, //0:反对 1:推荐 2:不反对,不推荐
+      collection: null, //收藏
+      attention: null, //关注
+
       last_article: {
         article_id: null,
         title: '',
