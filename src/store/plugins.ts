6a4431079fe0ae392c5c4012c2cdb730982972e2
dreamy-xay <1985332264@qq.com>
2022-01-03 21:50:45 +0800

    init store

---
+++ src/store/plugins.ts
@@ -0,0 +1,27 @@
+/*
+ * @Description: vuex 插件
+ * @Version:
+ * @Autor: dreamy-xay
+ * @Date: 2022-01-03 21:29:53
+ * @LastEditors: dreamy-xay
+ * @LastEditTime: 2022-01-03 21:45:16
+ */
+import { createLogger } from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+import { persistedstatePath } from './modules';
+
+// 调试模式下使用日志插件
+const debug = process.env.NODE_ENV !== 'production';
+
+// 特殊情况下使用的插件
+const constraintPlugins = [...(debug ? [createLogger({})] : [])];
+
+const plugins = [
+  ...constraintPlugins,
+  createPersistedState({
+    key: 'nsblog',
+    paths: persistedstatePath
+  })
+];
+
+export default plugins;
