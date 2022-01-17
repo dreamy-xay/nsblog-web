640da9cb006a121a25d310978e6c73378d08d414
dreamy-xay <1985332264@qq.com>
2022-01-17 23:32:44 +0800

    fix some bugs, add BaseTopicBar.vue

--- src/views/userCenter/childComps/pages/UserCenterProfileEdit/UserCenterProfileEdit.vue
+++ src/views/userCenter/childComps/pages/UserCenterProfileEdit/UserCenterProfileEdit.vue
@@ -3,8 +3,8 @@
  * @Version:
  * @Autor: Ban
  * @Date: 2021-08-19 11:57:31
- * @LastEditors: Ban
- * @LastEditTime: 2022-01-14 16:07:28
+ * @LastEditors: dreamy-xay
+ * @LastEditTime: 2022-01-17 23:24:56
 -->
 <template>
   <div class="user-center-profile-edit">
@@ -22,9 +22,10 @@
       @changeJob="changeJob"
     />
     <user-center-profile-edit-interest
-      :data="tagData"
-      id="userCenterProfileEditInteres"
+      :selected-tags="tagsData.tags"
       @deleteTag="deleteTag"
+      @addTag="addTag"
+      @updateTags="updateTags"
     />
   </div>
 </template>
@@ -119,8 +120,8 @@ export default defineComponent({
       };
     });
 
-    //标签
-    const tagData = computed(() => {
+    // 兴趣标签
+    const tagsData = computed(() => {
       return {
         username: userData.username,
         tags: userData.tags,
@@ -213,13 +214,36 @@ export default defineComponent({
 
     /**
      * @description: 删除兴趣标签
-     * @param {index} 标签索引
+     * @param {number} index 标签索引 `必传参数`
      * @author: Ban
      */
-
     function deleteTag(index) {
       userData.tags.splice(index, 1);
-      console.log(userData.tags);
+    }
+
+    /**
+     * @description: 添加兴趣标签
+     * @param {number} index 标签索引 `必传参数`
+     * @author: Ban
+     */
+    function addTag(tag) {
+      userData.tags.splice(userData.tags.length, 0, tag);
+    }
+
+    /**
+     * @description: 更改兴趣标签信息
+     * @return {void}
+     * @author: dreamy-xay
+     */
+    function updateTags() {
+      putUserInfo(tagsData)
+        .then(() => {
+          msg.success('修改成功', { duration: 2000, closable: true });
+        })
+        .catch((error) => {
+          console.log(error);
+          msg.error('修改兴趣标签失败', { duration: 2000, closable: true });
+        });
     }
 
     return {
@@ -230,12 +254,14 @@ export default defineComponent({
       informationData,
       userData,
       jobData,
-      tagData,
+      tagsData,
 
       changeInformation,
       changeJob,
 
       deleteTag,
+      addTag,
+      updateTags,
     };
   },
 });
