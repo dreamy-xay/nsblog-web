9f8e931ed1262237b3b1261c486d9da48236c571
KeepCoding <1710256504@qq.com>
2022-01-17 17:38:41 +0800

    update BlogCategories

--- src/views/blog/childComps/pages/childComps/BlogCategoriesChart.vue
+++ src/views/blog/childComps/pages/childComps/BlogCategoriesChart.vue
@@ -4,7 +4,7 @@
  * @Autor: clq
  * @Date: 2022-01-13 14:38:10
  * @LastEditors: clq
- * @LastEditTime: 2022-01-14 14:43:26
+ * @LastEditTime: 2022-01-17 17:01:43
 -->
 <template>
   <div class="blog-categories-chart">
@@ -17,7 +17,7 @@
 </template>
 
 <script>
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, onMounted, reactive, watch } from 'vue';
 
 /**
  * @description:
@@ -27,23 +27,73 @@ import { computed, defineComponent } from 'vue';
 export default defineComponent({
   name: 'blogCategoriesChart',
   props: {
-    chartData: {
-      type: Object,
+    indicator: {
+      type: Array,
       required: true,
+      default: () => [
+        { name: 'Sales', max: 6500 },
+        { name: 'Administration', max: 16000 },
+        { name: 'Information Technology', max: 30000 },
+        { name: 'Customer Support', max: 38000 },
+        { name: 'Development', max: 52000 },
+        { name: 'Marketing', max: 25000 },
+      ],
+    },
+    chartData: {
+      type: Array,
+      required: true,
+      default: () => [4200, 3000, 20000, 35000, 50000, 18000],
     },
   },
   setup(props) {
+    let indicator = reactive([
+      { name: 'Sales', max: 6500 },
+      { name: 'Administration', max: 16000 },
+      { name: 'Information Technology', max: 30000 },
+      { name: 'Customer Support', max: 38000 },
+      { name: 'Development', max: 52000 },
+      { name: 'Marketing', max: 25000 },
+    ]);
+
+    let value = reactive([4200, 3000, 20000, 35000, 50000, 18000]);
+
+    // indicator.slice(0, 0, ...props.indicator);
+    // value.slice(0, 0, ...props.chartData);
+
     //计算图标选项
     const option = computed(() => {
+      // let indicator1 = [];
+      // let value1 = [];
+      console.log('props.indicator');
+      console.log(props.indicator);
+      console.log('props.chartData');
+      console.log(props.chartData);
+      // indicator1.slice(0, 0, ...props.indicator);
+      // value1.slice(0, 0, ...props.chartData);
+
+      // for (const item of props.indicator) {
+      //   indicator1.push(item);
+      // }
+      // for (const item of props.chartData) {
+      //   value1.push(item);
+      // }
+
       return {
         radar: {
-          // shape: 'circle',
-          indicator: props.chartData.indicator,
+          indicator,
+          // indicator: indicator1,
+          // indicator: props.indicator,
         },
         series: [
           {
             type: 'radar',
-            data: props.chartData.data,
+            data: [
+              {
+                value,
+                // value: value1,
+                // value: props.chartData,
+              },
+            ],
           },
         ],
       };
