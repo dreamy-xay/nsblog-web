7ca3bd9499fb0929dc3ef0dab9fecc142b8996f1
xxb <1961141266@qq.com>
2022-01-19 20:45:15 +0800

    'update'

--- src/components/content/baseFooter/BaseFooter.vue
+++ src/components/content/baseFooter/BaseFooter.vue
@@ -4,46 +4,88 @@
  * @Autor: dreamy-xay
  * @Date: 2022-01-10 20:39:59
  * @LastEditors: xiao
- * @LastEditTime: 2022-01-19 16:06:38
+ * @LastEditTime: 2022-01-19 18:43:05
 -->
 
 <template>
   <div class="base-footer">
-    <div
-      class="foot1"
-      v-if="true"
-    >
-      <a href="">关于</a>
-      <a href="">博客</a>
-      <a
-        href=""
-        class="a3"
-      >网站</a>
+    <div v-if="isShow">
+      <div class="base-footer-first">
+        <a
+          v-for="(content,index) in contents"
+          :key="index"
+          :href="content.url"
+        >
+          {{content.name}}
+        </a>
+      </div>
+      <div class="base-footer-second">
+        <a
+          :href="title.url"
+          v-for="(title,index) in titles"
+          :key="index"
+        >
+          <i :class="title.icon"></i>
+        </a>
+      </div>
     </div>
-    <div
-      class="foot2"
-      v-if="true"
-    >
-      <a href=""><i class="iconfont blog-weibo"></i></a>
-      <a href=""> <i class="iconfont blog-email"></i></a>
+
+    <div class="base-footer-third">
+      <div>© 2021 ·</div>
+      <a href="">笔记分享部落阁</a>
+      <div>· ALL RIGHTS RESERVED</div>
     </div>
-    <div class="foot3">© 2021 · 笔记分享部落阁 · ALL RIGHTS RESERVED</div>
   </div>
 
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 /**
  * @description: 页脚
+ * @param {Boolean} isShow 是否显示前两行 `默认为true`
  * @author: dreamy-xay
  */
 
 export default defineComponent({
   name: 'baseFooter',
   setup() {
-    return {};
+    const isShow = ref(true); //是否显示前两行
+    const titles = [
+      //图标
+      {
+        name: '微博',
+        url: '',
+        icon: 'iconfont blog-weibo',
+      },
+      {
+        name: '邮箱',
+        url: '',
+        icon: 'iconfont blog-email',
+      },
+    ];
+    const contents = [
+      //第一行文字
+      {
+        name: '关于',
+        url: '',
+      },
+      {
+        name: '博客',
+        url: '',
+      },
+      {
+        name: '网站',
+        url: '',
+      },
+    ];
+
+    return {
+      isShow,
+      titles,
+      contents,
+    };
   },
 });
 </script>
@@ -56,36 +98,49 @@ export default defineComponent({
   box-shadow: $shadow-0;
   margin-top: 20px;
   @include flex(center, center, column);
-  .foot1 {
+
+  .base-footer-first {
     font-size: 15px;
     height: 20px;
     margin-top: 16px;
+
     a {
       color: $grey-7;
       margin-right: 20px;
-    }
-    .a3 {
-      margin-right: 0px;
+
+      &:last-child {
+        margin-right: 0px;
+      }
     }
   }
-  .foot2 {
+
+  .base-footer-second {
     height: 28px;
     margin-top: 10px;
     margin-bottom: 14px;
     @include flex(center, center);
+
     .blog-weibo {
       margin-right: 20px;
     }
+
     .iconfont {
       font-size: 20px;
       color: $grey-8;
     }
   }
-  .foot3 {
+
+  .base-footer-third {
+    @include flex(center, center, row);
     font-size: 14px;
     color: $grey-7;
     height: 20px;
     margin-bottom: 14px;
+
+    a {
+      color: $grey-7;
+      margin: 0px 5px;
+    }
   }
 }
 </style>
