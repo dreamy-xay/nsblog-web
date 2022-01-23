553922c920352aff8663e200d66ec89490f7509a
KeepCoding <1710256504@qq.com>
2022-01-23 20:40:26 +0800

    finish SearchResource

--- src/network/api/questions.ts
+++ src/network/api/questions.ts
@@ -4,44 +4,12 @@
  * @Autor: Z_Y_C
  * @Date: 2021-09-17 20:17:15
  * @LastEditors: clq
- * @LastEditTime: 2022-01-22 15:52:55
+ * @LastEditTime: 2022-01-22 17:21:22
  */
 import { get, RequestLifeCycle } from '@/network/request';
 
 /**
- * @description: 获取提问记录信息
- * @param {string} username 用户名
- * @param {number} offset 数据库提问记录偏移量 `默认为0`
- * @param {number} limit 返回提问记录的最大数量 `默认位10`
- * @param {number} release_time 按发布时间排序 1 升序 0 不排 -1 降序 `默认为1`
- * @param {number} browsing_count 按访问量排序 1 升序 0 不排 -1 降序 `默认位10`
- * @param {RequestLifeCycle} RLC 请求生命周期 `默认值为 {}`
- * @return {Promise<unknown>} 请求返回promise
- * @author: Z_Y_C
- */
-// export function getQuestions(
-//   username: string,
-//   offset: number = 0,
-//   limit: number = 10,
-//   release_time: number = 1,
-//   browsing_count: number = 0,
-//   RLC: RequestLifeCycle = {}
-// ): Promise<unknown> {
-//   return get({
-//     url: '/questions',
-//     ...RLC,
-//     params: {
-//       username,
-//       offset,
-//       limit,
-//       release_time,
-//       browsing_count
-//     }
-//   });
-// }
-
-/**
- * @description:
+ * @description: 获取问答
  * @param {string} username 用户名 `可选参数`
  * @param {number} release_time 按发布时间排序 1 升序 0 不排 -1 降序 `默认为1`
  * @param {number} browsing_count 按访问量排序 1 升序 0 不排 -1 降序 `默认位10`
@@ -55,12 +23,12 @@ import { get, RequestLifeCycle } from '@/network/request';
  * @author: clq
  */
 export function getQuestions(
-  username?: string,
-  release_time?: number,
-  browsing_count?: number,
-  type?: number,
-  topic_name?: string,
-  tag_name?: string,
+  username: string = '',
+  release_time: number = 0,
+  browsing_count: number = 0,
+  type: number = 0,
+  topic_name: string = '',
+  tag_name: string = '',
   limit: number = 10,
   offset: number = 0,
   RLC: RequestLifeCycle = {}
