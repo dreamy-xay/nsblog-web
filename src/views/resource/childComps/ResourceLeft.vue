3039f5e52c0147e6347442ae4e9684ffe7559f7e
Z_Y_C <2918765917@qq.com>
2022-01-23 16:11:01 +0800

    'update'

--- src/views/resource/childComps/ResourceLeft.vue
+++ src/views/resource/childComps/ResourceLeft.vue
@@ -4,11 +4,11 @@
  * @Autor: Z_Y_C
  * @Date: 2022-01-22 12:12:35
  * @LastEditors: Z_Y_C
- * @LastEditTime: 2022-01-22 19:55:42
+ * @LastEditTime: 2022-01-23 15:59:25
 -->
 <template>
   <div class="resource-left">
-
+    <div class="resource-left-top"></div>
   </div>
 </template>
 <script>
@@ -30,10 +30,10 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .resource-left {
-  width: 100%;
+  width: calc(100% - 40px);
   border-radius: $border-radius-0;
   box-shadow: $shadow-0;
   background-color: $grey-0;
-  height: 100px;
+  padding: 16px 20px;
 }
 </style>
