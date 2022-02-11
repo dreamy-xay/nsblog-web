f58ce95b20ace585f8cfe7799c50bec383a4b754
Z_Y_C <2918765917@qq.com>
2022-02-11 16:46:20 +0800

    '更新了一些'

--- src/network/api/notices.ts
+++ src/network/api/notices.ts
@@ -1,23 +1,22 @@
 /*
- * @Description: 获取登录日志api
+ * @Description:公告活动api
  * @Version:
  * @Autor: Z_Y_C
- * @Date: 2021-09-24 19:29:33
+ * @Date: 2022-02-11 14:52:17
  * @LastEditors: Z_Y_C
- * @LastEditTime: 2022-02-11 16:27:57
+ * @LastEditTime: 2022-02-11 15:50:26
  */
-
 import { get, RequestLifeCycle } from '@/network/request';
 
 /**
- * @description: 获取登录日志
+ * @description: 获取公告活动api
  * @param {RequestLifeCycle} RLC 请求生命周期 `默认值为 {}`
  * @return {Promise<unknown>} 请求返回promise
  * @author: Z_Y_C
  */
-export function getLogs(RLC: RequestLifeCycle = {}): Promise<unknown> {
+export function getNotices(RLC: RequestLifeCycle = {}): Promise<unknown> {
   return get({
-    url: '/logs/login',
+    url: '/notices',
     ...RLC
   });
 }
