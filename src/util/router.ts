35d632f231d0f120849cb804929b86893e53f3c3
dreamy-xay <1985332264@qq.com>
2022-03-20 23:22:42 +0800

    add method searchMenuRoutes, fix some bugs

--- src/util/router.ts
+++ src/util/router.ts
@@ -4,7 +4,7 @@
  * @Autor: dreamy-xay
  * @Date: 2022-02-26 19:51:18
  * @LastEditors: dreamy-xay
- * @LastEditTime: 2022-03-20 22:34:11
+ * @LastEditTime: 2022-03-20 23:18:29
  */
 
 import router from '@/router';
@@ -76,7 +76,10 @@ export function searchMenuRoutes(
     menuRoute.push(route);
     let ok: boolean = false; // 下一级是否已经收集
     for (const r of route.children) if (filterMenu(r)) ok = true;
-    if ((!ok || all) && rules(route)) menuRoutes.push(menuRoute);
+    if ((!ok || all) && rules(route)) {
+      ok = true;
+      menuRoutes.push(JSON.parse(JSON.stringify(menuRoute)));
+    }
     menuRoute.pop();
     return ok;
   }
